939b4ed302ae8b8c127fa3fc7885a375b8cebc0c
RobertFrundin <robert.frundin@gmail.com>
2022-11-02 23:04:40 +0300

    removed await

--- api/editTicket.js
+++ api/editTicket.js
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const fetch = require('node-fetch')
 const { AUTH_TOKEN, ORG_ID, TICKET_QUERYPARAM } = process.env
-await fetch(`https://api.tracker.yandex.net/v2/issues/${TICKET_QUERYPARAM}/comments`, {
+fetch(`https://api.tracker.yandex.net/v2/issues/${TICKET_QUERYPARAM}/comments`, {
   method: 'PATCH',
   headers: {
     Authorization: `OAuth ${AUTH_TOKEN}`,
