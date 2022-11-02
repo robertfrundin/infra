264c2a8e39c14b8fe3c87fd19a05d4ff71f33dca
RobertFrundin <robert.frundin@gmail.com>
2022-11-03 00:58:46 +0300

    release number test

--- api/editTicket.js
+++ api/editTicket.js
@@ -1,7 +1,11 @@
 /* eslint-disable no-undef */
 /* eslint-disable @typescript-eslint/no-var-requires */
 import fetch from 'node-fetch'
+import github from '@actions/github'
 let { AUTH_TOKEN, ORG_ID, TICKET_QUERYPARAM } = process.env
+console.log('1. Getting current ref:')
+const tag = github.context.ref.split('/').pop()
+const releaseNumber = getReleaseNumber(tag)
 
 fetch(`https://api.tracker.yandex.net/v2/issues/${TICKET_QUERYPARAM}`, {
   method: 'PATCH',
@@ -10,7 +14,7 @@ fetch(`https://api.tracker.yandex.net/v2/issues/${TICKET_QUERYPARAM}`, {
     'X-Org-ID': ORG_ID,
   },
   body: JSON.stringify({
-    summary: 'FETCH CHEEECK',
+    summary: `Релиз ${releaseNumber}`,
   }),
 })
   .then((res) => res.text())
